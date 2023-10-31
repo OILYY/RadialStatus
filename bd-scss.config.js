c8e7bebff51dbd1af1e97c9da297e3d66752d9d9
OILYY <124994484+OILYY@users.noreply.github.com>
2023-10-31 14:49:59 -0600

    Add files via upload

---
+++ bd-scss.config.js
@@ -0,0 +1,16 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: 'RadialStatus',
+		author: 'OilyGranySmith',
+		version: '1.0.0',
+		description:
+			'Changes the status icons to wrap around the avatar. Shape can be changed inside the theme file.',
+		source: 'https://github.com/OILYY/RadialStatus',
+		invite: 'Y36CTWeCFE'
+	},
+	base: {
+		output: 'dist/dist'
+	},
+	baseImport: 'https://OILYY.github.io/RadialStatus/dist/RadialStatus.css'
+};
